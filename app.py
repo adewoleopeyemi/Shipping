0d81ea4a2e1ebe67059d6890a5fd7a80574ba6c0
Adewole Opeyemi <58083371+adewoleopeyemi@users.noreply.github.com>
2021-05-27 21:43:54 -0700

    updated threshold

--- app.py
+++ app.py
@@ -72,11 +72,11 @@ def predict_image_file():
         200:
             description: The output values
     '''
-    model = load_model("trainedmodelsh5/vggnuditymodel.h5")
+    model = load_model("trainedmodelsh5/vggnuditymodel (1).h5")
     img = preprocess_one_image(request.files['file'])
     prediction = model.predict(img.reshape((1, 124, 124, 3)))
-    if prediction >0.50:
-        return "Nudity detected "
+    if prediction <= 0.50:
+        return "Nudity detected, please do not view if you are under 18"
     else:
         return "The uploaded image doesn't contain any form of nudity you look good to go"
 
