3dece1f650ab62a45ac5ac7b315be1808e66a7da
Adewole Opeyemi <58083371+adewoleopeyemi@users.noreply.github.com>
2021-05-27 21:18:51 -0700

    updated threshold

--- app.py
+++ app.py
@@ -75,8 +75,8 @@ def predict_image_file():
     model = load_model("trainedmodelsh5/vggnuditymodel.h5")
     img = preprocess_one_image(request.files['file'])
     prediction = model.predict(img.reshape((1, 124, 124, 3)))
-    if prediction >0.60:
-        return "Nudity detected with percentage confidence of " + str(prediction)
+    if prediction >0.50:
+        return "Nudity detected "
     else:
         return "The uploaded image doesn't contain any form of nudity you look good to go"
 
